797a53d57ce2ca68ac931a16bb3aa3b6e6183cab
wadax7777 <github@wadax.com>
2026-07-18 11:00:39 +0900

    Ver16 ベースファイル登録

---
+++ config-public.js
@@ -0,0 +1,9 @@
+/* NEGAIBOSHI.SITE Ver.12 FREE MAX
+   空欄のままでも動作します。
+   Cesium ion無料アカウントのトークンを入れるとWorld Terrainが有効になります。
+   Bing Mapsキーをお持ちの場合だけ bingMapsKey に設定してください。
+*/
+window.NEGAIBOSHI_CONFIG = {
+  cesiumIonToken: "",
+  bingMapsKey: ""
+};
